09e038e63d8e46ced2cf55d6551f487f58b10a1b
Muhamd-ilham <ilhamzkotlin@gmail.com>
2026-02-16 14:04:39 +0700

    Ubah koneksi sama nambah Index html

--- koneksi.php
+++ koneksi.php
@@ -4,11 +4,11 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 
 // --- KONFIGURASI DATABASE ---
-$host = "127.0.0.1";  // Ganti 'localhost' jadi IP ini biar lebih stabil di port khusus
-$port = 3307;         // <--- INI KUNCINYA (Sesuai screenshot Abang)
-$user = "root";       // User default (Sesuai screenshot: root)
+$host = "localhost";  // Ganti 'localhost' jadi IP ini biar lebih stabil di port khusus
+$port = 3306;         // <--- INI KUNCINYA (Sesuai screenshot Abang)
+$user = "";       // User default (Sesuai screenshot: root)
 $pass = "";           // Password kosong (Sesuai screenshot: Tidak ada sandi)
-$db   = "wedding_db"; // Nama database
+$db   = ""; // Nama database
 
 // Buat koneksi dengan parameter Port (Parameter ke-5)
 $conn = new mysqli($host, $user, $pass, $db, $port);
